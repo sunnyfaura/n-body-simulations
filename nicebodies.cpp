897258e83787c2076539e5b0109b303a34435c0e
Christine Tero <christine.tero@gmail.com>
2012-12-04 16:32:41 +0800

    not important

--- nicebodies.cpp
+++ nicebodies.cpp
@@ -8,15 +8,25 @@
 using namespace std;
 
 const double SPF = 1.0/60.0;
+const double bounceSpeed = 0.01;
+const int MAX = 50;
 
 #define MAT_SET_TRANSLATE(M, x, y) { M[6] = (x); M[7] = (y); }
 
-const double bounceSpeed = 0.01;
+struct Particle{
+	double x, y;
+};
+
+Particle arr[MAX];
+
+float fRand() {
+	return (float)rand()/RAND_MAX;
+}
 
 int main() {
 	glfwInit();
 
-	glfwOpenWindow(300, 300, 8, 8, 8, 8, 0, 0, GLFW_WINDOW);
+	glfwOpenWindow(600, 600, 8, 8, 8, 8, 0, 0, GLFW_WINDOW);
 
 	glewExperimental = GL_TRUE;
 	glewInit();
@@ -48,7 +58,11 @@ int main() {
 	double dir = 1;
 
 	//generate more of these coordinates with random initial position
-	double x = 0, y = 0;
+	int i = 0;
+	for(; i < MAX; i++){
+			arr[i].x = fRand() - 0.5;
+			arr[i].y = fRand() - 0.5;	
+	}
 	
 	while ( glfwGetWindowParam( GLFW_OPENED ) && !glfwGetKey(GLFW_KEY_ESC) ) {
 		const double cur = glfwGetTime();
@@ -69,35 +83,53 @@ int main() {
 		//generate x and y coordinates by numerical approx
 		//similar to Reimann's Sum
 
+		
 		while ( acc >= SPF ) {
 			acc -= SPF;
 			
 			//update
 			t += SPF;
 
-			x += (dir * bounceSpeed);// * SPF;
-			
-			if ( x > 1 ) {
-				x = 1;
-				dir *= -1;
-			} else if ( x < -1 ) {
-				x = -1;
-				dir *= -1;
+			for(i = 0; i < MAX; ++i){
+				arr[i].x += (sin((arr[i].x))*bounceSpeed*dir);// * SPF;
+				arr[i].y += (sin((arr[i].y))*bounceSpeed*dir);// * bounceSpeed);
+				
+				if ( arr[i].x > 1 ) {
+					//arr[i].x = 1;
+					dir *= -1;
+				} else if ( arr[i].x < -1 ) {
+					//arr[i].x = -1;
+					dir *= -1;
+				}
+				if ( arr[i].y > 1 ) {
+					//arr[i].y  = 1;
+					 dir *= -1;
+				} else if ( arr[i].y < -1 ) {
+					//arr[i].y = -1;
+					 dir *= -1;
+				}
+
+				if ( arr[i].x < 0.00000000000001 ) {
+					//arr[i].x = 1;
+					dir *= -1;
+				} 
+				if ( arr[i].y < 0.00000000000001 ) {
+					//arr[i].y  = 1;
+					 dir *= -1;
+				} 
 			}
 		}
 
 		//draw
 		glClearColor(0, 0, 0, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);	
 
-		//create a for loop that will translate, shade, and draw per circle
-		MAT_SET_TRANSLATE(mat, x, y);
-		glUniformMatrix3fv(U_TRANSFORM, 1, GL_FALSE, mat);
-		p.draw();
-
-		glfwSwapBuffers();
-		
-		
+		for(i = 0; i < MAX; i++){
+			MAT_SET_TRANSLATE(mat, arr[i].x, arr[i].y);
+			glUniformMatrix3fv(U_TRANSFORM, 1, GL_FALSE, mat);
+			p.draw();
+		}
+		glfwSwapBuffers();			
 	}	
 
 	glfwTerminate();
