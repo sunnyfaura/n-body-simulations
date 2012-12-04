00e2913e119e4282abb59730618afb5ca5165575
Marione John R. Semilla <marione-jrs312@hotmail.com>
2012-12-04 23:11:54 +0800

    hirap

--- Polygon.h
+++ Polygon.h
@@ -27,7 +27,7 @@ class Polygon {
 public:
 	enum { ATTR_POS, ATTR_COLOR };
 
-	Polygon(float radius, int vertices, GLuint color, float initialAngle = 0) : totalVertices(vertices + 2) {
+	Polygon(float radius, float xOffset, float yOffset, int vertices, GLuint color, float initialAngle = 0) : totalVertices(vertices + 2) {
 		glGenBuffers(1, &vbo);
 		glGenVertexArrays(1, &vao);
 
@@ -37,15 +37,16 @@ public:
 		glBufferData(GL_ARRAY_BUFFER, sizeof(Vtx) * totalVertices, 0, GL_STATIC_DRAW);
 		Vtx *v = (Vtx*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
 
-		v[0].x = v[0].y = 0;
+		v[0].x = xOffset;
+		v[0].y = yOffset;
 		v[0].color = color;
 
 		const float increment = 2 * M_PI /vertices;
 		for ( int i = 1; i < vertices + 2; ++i ) {
 			const float t = initialAngle + increment * (i - 1);
 			v[i].color = color;
-			v[i].x = cos(t) * radius;
-			v[i].y = sin(t) * radius;
+			v[i].x = cos(t) * radius + xOffset;
+			v[i].y = sin(t) * radius + yOffset;
 		}
 		glUnmapBuffer(GL_ARRAY_BUFFER);
 
@@ -60,7 +61,8 @@ public:
 		glDeleteBuffers(1, &vbo);
 	}
 	
-	void draw() {
+	void draw(GLuint TRANSFORM_ID, GLfloat* mat) {
+		glUniformMatrix3fv(TRANSFORM_ID, 1, GL_FALSE, mat);
 		glBindVertexArray(vao);
 		glDrawArrays(GL_TRIANGLE_FAN, 0, totalVertices);
 	}
--- nicebodies.cpp
+++ nicebodies.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
 #include "shader_helper.h"
 #include "Polygon.h"
@@ -10,19 +11,33 @@ using namespace std;
 const double SPF = 1.0/60.0;
 const double bounceSpeed = 0.01;
 const int MAX = 50;
+const int CIRCLE_VERT = 20;
+const double MASS_CONST = 1.0;
+const double K_CONST = 5.0;
 
 #define MAT_SET_TRANSLATE(M, x, y) { M[6] = (x); M[7] = (y); }
 
+/**
+GRAVITATIONAL FORCE = k ( m1 , m2 ) / r * r
+r is the distance between the bodies (IS A VECTOR) 
+
+
+**/
+
 struct Particle{
-	double x, y;
+	double x, y, currPosX, currPosY,
+		   mass, accelX, accelY,
+		   force;
+
 };
 
 Particle arr[MAX];
 
 float fRand() {
 	return (float)rand()/RAND_MAX;
+	//return -1.0f + (GLfloat) rand()/ ((GLfloat) RAND_MAX/(2));
 }
-
+1
 int main() {
 	glfwInit();
 
@@ -32,7 +47,9 @@ int main() {
 	glewInit();
 
 	//generate more circles
-	Polygon p(0.05, 20, 0xFFFF00FF);
+	
+
+	Polygon p(0.05, 0, 0.1, CIRCLE_VERT, 0xFFFF00FF);
 
 	GLuint mainProgram;
 
@@ -61,7 +78,8 @@ int main() {
 	int i = 0;
 	for(; i < MAX; i++){
 			arr[i].x = fRand() - 0.5;
-			arr[i].y = fRand() - 0.5;	
+			arr[i].y = fRand() - 0.5;
+			arr[i].mass = fRand() - 0.5;	
 	}
 	
 	while ( glfwGetWindowParam( GLFW_OPENED ) && !glfwGetKey(GLFW_KEY_ESC) ) {
@@ -83,17 +101,20 @@ int main() {
 		//generate x and y coordinates by numerical approx
 		//similar to Reimann's Sum
 
-		
+		double unitX, unitY, distX, distY, magDist;
 		while ( acc >= SPF ) {
 			acc -= SPF;
 			
 			//update
 			t += SPF;
 
-			for(i = 0; i < MAX; ++i){
-				arr[i].x += (sin((arr[i].x))*bounceSpeed*dir);// * SPF;
-				arr[i].y += (sin((arr[i].y))*bounceSpeed*dir);// * bounceSpeed);
+			for(i = 0; i < MAX - 1; ++i){
 				
+				//arr[i].x =(sin((arr[i].x))*bounceSpeed*dir);// * SPF;
+				//arr[i].y =(sin((arr[i].y))*bounceSpeed*dir);// * bounceSpeed);
+				
+#pragma region STUFF
+				/**		
 				if ( arr[i].x > 1 ) {
 					//arr[i].x = 1;
 					dir *= -1;
@@ -116,7 +137,36 @@ int main() {
 				if ( arr[i].y < 0.00000000000001 ) {
 					//arr[i].y  = 1;
 					 dir *= -1;
-				} 
+				}
+				**/
+#pragma endregion
+
+				for (int j = 1 ; j < MAX; j++)
+				{ 
+					
+					distX = arr[i].currPosX - arr[j].currPosX;
+					distY = arr[i].currPosY - arr[j].currPosY;
+					magDist = (distX * distX) + (distY * distY);
+					unitX = distX / sqrt(magDist);
+					unitY = distY / sqrt(magDist);
+					
+					arr[i].force = arr[j].force = ((-K_CONST * arr[i].mass * arr[j].mass) / magDist);
+					
+					arr[i].accelX = (arr[i].force/arr[i].mass) ;
+					arr[i].accelY = (arr[i].force/arr[i].mass) ;
+
+					arr[j].accelX = (arr[j].force/arr[j].mass) ;
+					arr[j].accelY = (arr[j].force/arr[j].mass) ;
+
+					arr[i].x += arr[i].accelX * SPF;
+					arr[i].y += arr[i].accelY * SPF;
+					
+					arr[j].x += arr[j].accelX * SPF;
+					arr[j].y += arr[j].accelY * SPF;
+					//arr[i].forceY = ((-K_CONST * arr[i].mass * arr[j].mass) / magDist);
+
+				}
+				
 			}
 		}
 
@@ -126,8 +176,8 @@ int main() {
 
 		for(i = 0; i < MAX; i++){
 			MAT_SET_TRANSLATE(mat, arr[i].x, arr[i].y);
-			glUniformMatrix3fv(U_TRANSFORM, 1, GL_FALSE, mat);
-			p.draw();
+			//glUniformMatrix3fv(U_TRANSFORM, 1, GL_FALSE, mat);
+			p.draw(U_TRANSFORM,mat);
 		}
 		glfwSwapBuffers();			
 	}	
